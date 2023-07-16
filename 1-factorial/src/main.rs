408f56323ca2556f881b19741991a4eb77d39f28
trybick <t.rybicki@gmail.com>
2023-07-16 19:52:07 -0400

    Refactor factorial

--- 1-factorial/src/main.rs
+++ 1-factorial/src/main.rs
@@ -1,13 +1,32 @@
-use std::env;
+use std::io;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let max_number = &args[1].parse::<i32>().unwrap();
+fn factorial(number: u64) -> u64 {
     let mut result = 1;
-
-    for i in 2..=*max_number {
+    for i in 2..=number {
         result = result * i;
     }
+    result
+}
 
-    println!("The factorial is {}", result);
+fn main() {
+    println!("Enter a number:");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Invalid input");
+
+    let number = input.trim().parse::<u64>().expect("That's not a number");
+
+    let result = factorial(number);
+
+    println!("The factorial {} is {}", input, result);
+
+
+    // let args: Vec<String> = env::args().collect();
+    // let max_number = &args[1].parse::<i32>().unwrap();
+    // let mut result = 1;
+
+    // for i in 2..=*max_number {
+    //     result = result * i;
+    // }
+
+    // println!("The factorial is {}", result);
 }
