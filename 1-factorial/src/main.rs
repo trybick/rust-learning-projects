4973ab9e0d218736f48ebfff1a22bc36402598a1
trybick <t.rybicki@gmail.com>
2023-07-15 20:06:38 -0400

    Complete 1-factorial

--- 1-factorial/src/main.rs
+++ 1-factorial/src/main.rs
@@ -2,5 +2,12 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    let max_number = &args[1].parse::<i32>().unwrap();
+    let mut result = 1;
+
+    for i in 2..=*max_number {
+        result = result * i;
+    }
+
+    println!("The factorial is {}", result);
 }
