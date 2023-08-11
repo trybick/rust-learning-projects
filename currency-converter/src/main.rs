5b4b93791bd39919f9ec7f01497a82b0f9fac471
trybick <t.rybicki@gmail.com>
2023-08-11 16:16:17 -0400

    Get select cli working

--- currency-converter/src/main.rs
+++ currency-converter/src/main.rs
@@ -1,3 +1,23 @@
+use dialoguer::{theme::ColorfulTheme, Select};
+
+// Develop a programme to convert currency X to currency Y and vice versa
+//
+// Input - ask do you want to convert FROM or TO currency
+// Input - enter amount
+// Do the conversion and output it
+
+
 fn main() {
-    println!("Hello, world!");
+    let options = &[
+        "USD to Chinese Yuan",
+        "Chinese Yuan to USD",
+    ];
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("Choose an option to convert:")
+        .default(0)
+        .items(&options[..])
+        .interact()
+        .unwrap();
+
+        println!("Enjoy your {}!", options[selection]);
 }
