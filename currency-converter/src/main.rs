af61427b4533c3e0f1e6f45da59dbcdc613074f2
trybick <t.rybicki@gmail.com>
2023-08-11 16:40:34 -0400

    Basically finish but erroring on run

--- currency-converter/src/main.rs
+++ currency-converter/src/main.rs
@@ -1,10 +1,7 @@
 use dialoguer::{theme::ColorfulTheme, Select};
 
-// Develop a programme to convert currency X to currency Y and vice versa
-//
-// Input - ask do you want to convert FROM or TO currency
-// Input - enter amount
-// Do the conversion and output it
+const EXCHANGE_RATE_USD_TO_CHINESE: f64 = 7.24;
+const EXCHANGE_RATE_CHINESE_TO_USD: f64 = 0.14;
 
 
 fn main() {
@@ -18,7 +15,18 @@ fn main() {
         .items(&options[..])
         .interact()
         .unwrap();
+    let is_usd_to_chinese = selection as i32 == 0;
 
-        let is_usd_to_chinese = selection as i32 == 0;
+    let mut amount = String::new();
+    println!("Enter amount:");
+    std::io::stdin().read_line(&mut amount).unwrap();
+
+    if is_usd_to_chinese {
+        let result = amount.parse::<f64>().unwrap() * EXCHANGE_RATE_USD_TO_CHINESE;
+        println!("{} USD converted to Chinese Yuan is {}", amount, result);
+    } else {
+        let result = amount.parse::<f64>().unwrap() * EXCHANGE_RATE_CHINESE_TO_USD;
+        println!("{} Chinese Yuan converted to USD is {}", amount, result);
+    }
 
 }
