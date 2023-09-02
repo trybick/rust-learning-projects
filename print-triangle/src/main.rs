6fd85b88cac00e8b891e95156a5a8f02402e868a
trybick <t.rybicki@gmail.com>
2023-09-02 15:23:12 -0400

    Create print-triangle project

---
+++ print-triangle/src/main.rs
@@ -0,0 +1,8 @@
+// 1. Write a programme that prints out a triangle from largest to smallest; user inputs the largest number
+// 2. Write a programme that prints out a triangle from smallest to largest; user inputs bottom number.
+// 3. Print out a triangle from smallest to largest, skipping even rows. User inputs largest number
+
+
+fn main() {
+    println!("Hello, world!");
+}
