5aa94431d65519ee70694af445f47a37a0923a29
trybick <t.rybicki@gmail.com>
2023-09-03 19:58:37 -0400

    Finish number guessing game

--- number-guessing-game/src/main.rs
+++ number-guessing-game/src/main.rs
@@ -1,3 +1,24 @@
+use rand::Rng;
+
+// Guessing game. ask the user to guess a number between 1 and a 100.
+// If you guessed correctly, it will say you win.
+// If you're too high or too low it will also let you know.
+
 fn main() {
-    println!("Hello, world!");
+    let answer = rand::thread_rng().gen_range(1..100);
+    println!("{}", answer);
+
+    println!("Try to guess the correct number! Please enter a number from 1 to 100: ");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let input_number: i32 = input.trim().parse().unwrap();
+
+    println!();
+    if input_number == answer {
+        println!("You win!");
+    } else if input_number > answer {
+        println!("Too high!");
+    } else {
+        println!("Too low!");
+    }
 }
